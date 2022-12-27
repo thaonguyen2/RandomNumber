cd77788979a5779150925809d4a4603e285092d4
Thao Nguyen <thaonh@tohsoft.com>
2022-12-27 14:20:24 +0700

    State management with riverpod

--- lib/randomizer_page.dart
+++ lib/randomizer_page.dart
@@ -1,39 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_foundations/randomizer_change_notifier.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_foundations/main.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class RandomizerPage extends StatelessWidget {
+class RandomizerPage extends ConsumerWidget {
   const RandomizerPage({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final randomizer = ref.watch(randomizerProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Randomizer'),
       ),
-      // body: Center(
-      //   child: Text(
-      //     context
-      //             .watch<RandomizerChangeNotifier>()
-      //             .generatedNumber
-      //             ?.toString() ??
-      //         'Generate a number',
-      //     style: const TextStyle(fontSize: 32),
-      //   ),
-      // ),
-      body: Center(child: Consumer<RandomizerChangeNotifier>(
-        builder: (context, notifier, child) {
-          return Text(
-            notifier.generatedNumber?.toString() ?? 'Generate a number',
-            style: const TextStyle(fontSize: 32),
-          );
-        },
-      )),
+      body: Center(
+        child: Text(
+          randomizer.generatedNumber?.toString() ?? 'Generate a number',
+          style: const TextStyle(fontSize: 32),
+        ),
+      ),
       floatingActionButton: FloatingActionButton.extended(
           onPressed: () {
-            context.read<RandomizerChangeNotifier>().generateRandomNumber();
+            randomizer.generateRandomNumber();
           },
           label: const Text('Generate')),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
--- lib/range_selector_form.dart
+++ lib/range_selector_form.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_foundations/randomizer_change_notifier.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_foundations/main.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 typedef IntValueSetter = void Function(int value);
 
-class RangeSelectorForm extends StatelessWidget {
+class RangeSelectorForm extends ConsumerWidget {
   const RangeSelectorForm({
     Key? key,
     required this.formKey,
@@ -13,7 +13,8 @@ class RangeSelectorForm extends StatelessWidget {
   final GlobalKey<FormState> formKey;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final randomizer = ref.read(randomizerProvider);
     return Form(
       key: formKey,
       child: Padding(
@@ -23,16 +24,14 @@ class RangeSelectorForm extends StatelessWidget {
           children: [
             RangeSelectorTextFormField(
               labelText: 'Minimum',
-              intValueSetter: (value) =>
-                  context.read<RandomizerChangeNotifier>().min = value,
+              intValueSetter: (value) => randomizer.min = value,
             ),
             const SizedBox(
               height: 16,
             ),
             RangeSelectorTextFormField(
               labelText: 'Maximum',
-              intValueSetter: (value) =>
-                  context.read<RandomizerChangeNotifier>().max = value,
+              intValueSetter: (value) => randomizer.max = value,
             ),
           ],
         ),
